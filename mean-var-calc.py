841b0c04c7fea92c2a4b680a8a03829f20706862
KadeNierman <126372815+KadeNierman@users.noreply.github.com>
2023-07-31 21:05:48 -0700

    Update mean-var-calc.py

--- mean-var-calc.py
+++ mean-var-calc.py
@@ -1,26 +1,41 @@
 import numpy as np
 
-# Get input from user for array numbers
-list = input('Please give 9 numbers: ')
-# Converts input to string and iterates over values adding commas
-nlist = [int(d) for d in str(list)]
 
-def calculate():
-    # If user gives 9 numbers, program will run
-    if len(list) == 9:
-        # Creates a flattened array of user input
-        arr = np.array(nlist)
-        # Create a 3,3 array
-        narr = arr.reshape(3, 3)
-        print(narr)
-        print('Mean: ', np.mean(narr, axis=0), np.mean(narr, axis=1), np.mean(narr))
-        print('Variance: ', np.var(narr, axis=0), np.var(narr, axis=1), np.var(narr), )
-        print('Standard deviation: ', np.std(narr, axis=0), np.std(narr, axis=1), np.std(narr), )
-        print('Max: ',  np.max(narr, axis=0), np.max(narr, axis=1), np.max(narr), )
-        print('Min: ', np.min(narr, axis=0), np.min(narr, axis=1), np.min(narr), )
-        print('Sum: ', np.sum(narr, axis=0),  np.sum(narr, axis=1), np.sum(narr))
-    #If user gives less or more than 9 numbers will be prompted to input 9
-    else:
-        print('Please enter 9 numbers!')
+def calculate(list):
 
-calculate()
+    ls = np.array(list)
+    print(ls)
+
+    if len(ls) != 9:
+        raise ValueError("List must contain nine numbers.")
+
+    mean_rows = [ls[[0, 1, 2]].mean(), ls[[3, 4, 5]].mean(), ls[[6, 7, 8]].mean()]
+    mean_columns = [ls[[0, 3, 6]].mean(), ls[[1, 4, 7]].mean(), ls[[2, 5, 8]].mean()]
+
+    variance_rows = [ls[[0, 1, 2]].var(), ls[[3, 4, 5]].var(), ls[[6, 7, 8]].var()]
+    variance_columns = [ls[[0, 3, 6]].var(), ls[[1, 4, 7]].var(), ls[[2, 5, 8]].var()]
+
+    standard_deviation_rows = [ls[[0, 1, 2]].std(), ls[[3, 4, 5]].std(), ls[[6, 7, 8]].std()]
+    standard_deviation_columns = [ls[[0, 3, 6]].std(), ls[[1, 4, 7]].std(), ls[[2, 5, 8]].std()]
+
+    max_rows = [ls[[0, 1, 2]].max(), ls[[3, 4, 5]].max(), ls[[6, 7, 8]].max()]
+    max_columns = [ls[[0, 3, 6]].max(), ls[[1, 4, 7]].max(), ls[[2, 5, 8]].max()]
+
+    min_rows = [ls[[0, 1, 2]].min(), ls[[3, 4, 5]].min(), ls[[6, 7, 8]].min()]
+    min_columns = [ls[[0, 3, 6]].min(), ls[[1, 4, 7]].min(), ls[[2, 5, 8]].min()]
+
+    sum_rows = [ls[[0, 1, 2]].sum(), ls[[3, 4, 5]].sum(), ls[[6, 7, 8]].sum()]
+    sum_columns = [ls[[0, 3, 6]].sum(), ls[[1, 4, 7]].sum(), ls[[2, 5, 8]].sum()]
+
+    return {
+        'mean': [mean_columns, mean_rows, ls.mean()],
+        'variance': [variance_columns, variance_rows, ls.var()],
+        'standard deviation': [standard_deviation_columns, standard_deviation_rows, ls.std()],
+        'max': [max_columns, max_rows, ls.max()],
+        'min': [min_columns, min_rows, ls.min()],
+        'sum': [sum_columns, sum_rows, ls.sum()]
+    }
+
+
+
+print(calculate([0,1,2,3,4,5,6,7,8]))
